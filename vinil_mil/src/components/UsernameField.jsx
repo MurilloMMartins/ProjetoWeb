f44e01cf2706930fb368922135210d714b5aaaa3
MurilloMMartins <murillomartins19@gmail.com>
2023-06-06 15:49:07 -0300

    Renamed UsernameField.css to modularize to any field created

--- vinil_mil/src/components/UsernameField.jsx
+++ vinil_mil/src/components/UsernameField.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './UsernameField.css'
+import './FormField.css'
 
 const UsernameField = ({children}) => {
     return (
         <div className="field-container">
-            <input type="text" className='field-text' placeholder={children}></input>
+            <input type="text" className='field-text' placeholder={children}/>
         </div>
     );
 }
