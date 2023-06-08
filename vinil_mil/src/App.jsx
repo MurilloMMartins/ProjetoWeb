fa06c1e1c6321d02ddf0590c12d2f4592ebcf321
MurilloMMartins <murillomartins19@gmail.com>
2023-06-08 18:05:43 -0300

    Updated login route

--- vinil_mil/src/App.jsx
+++ vinil_mil/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<LoginPage/>}/>
+                <Route path='/login' element={<LoginPage/>}/>
             </Routes>
         </BrowserRouter>
     );
