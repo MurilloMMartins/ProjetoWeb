c6126a8513e8c1dd6a6c2f6da7961c6e9b6e9467
MurilloMMartins <murillomartins19@gmail.com>
2023-06-08 17:12:28 -0300

    Added TODO in this source file

--- vinil_mil/src/components/AuthForm/index.jsx
+++ vinil_mil/src/components/AuthForm/index.jsx
@@ -4,6 +4,9 @@ import './AuthForm.css'
 import SubmitButton from './SubmitButton';
 
 const AuthForm = ({children, name}) => {
+    /**
+        @todo: Find a way to submit the login information to the parent
+    **/
     return (
         <div className="auth-form-container">
             <form onSubmit={console.log("submited")} className='auth-form'>
