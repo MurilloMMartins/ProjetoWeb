f641ca88f8e2192612ba789af965e4a8ed95bc40
MurilloMMartins <murillomartins19@gmail.com>
2023-06-08 18:06:00 -0300

    Removed unused imports

--- vinil_mil/src/components/AuthForm/index.jsx
+++ vinil_mil/src/components/AuthForm/index.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './AuthForm.css'
-import PasswordField from './PasswordField';
 import SubmitButton from './SubmitButton';
-import UsernameField from './UsernameField';
 
 const AuthForm = ({children, name, handleSubmit}) => {
     return (
