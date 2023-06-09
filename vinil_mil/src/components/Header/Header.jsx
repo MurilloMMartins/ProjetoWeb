5de5aeffed98b500dc4e99526a0b57edf622256b
MurilloMMartins <murillomartins19@gmail.com>
2023-06-09 11:06:01 -0300

    Placed require inside <img> tags to render images correctly

--- vinil_mil/src/components/Header/Header.jsx
+++ vinil_mil/src/components/Header/Header.jsx
@@ -8,13 +8,13 @@ function Header() {
     <header>
       <Link to='/home'>
         <div id="header-left">
-          <img id="logo-img" src="../../data/logo.png" alt="Logo Vinil Mil" />
+          <img id="logo-img" src={require("../../data/logo.png")} alt="Logo Vinil Mil" />
           <span id="logo-span">Vinil Mil</span>
         </div>
       </Link>
       <div id="header-center"></div>
       <div id="header-right">
-        <img id="profile-img" src="../../data/placeholders/default-profile.png" alt="Profile" />
+        <img id="profile-img" src={require("../../data/placeholders/default-profile.png")} alt="Profile" />
         <Link to='/login'><span id="login-span">Entrar/Registrar</span></Link>
         <Link to='/shopping-cart'><span id="shopping-cart-span">Carrinho</span></Link>
       </div>
