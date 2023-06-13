0949e608d480cd3aeb30dc230d4b3e2c858d8f1d
gabrielperao <gabrielperao@hotmail.com>
2023-06-13 17:44:21 -0300

    Added deletion logic to MenuAdminPage + vinyl listing

--- vinil_mil/src/pages/MenuAdminPage/index.jsx
+++ vinil_mil/src/pages/MenuAdminPage/index.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
-const MenuAdminPage = ({ curUser, allVinyls, setSelectedVinyls, addItemToShoppingCart }) => {
+const MenuAdminPage = ({ curUser, allVinyls, setAllVinyls, addItemToShoppingCart }) => {
     const navigate = useNavigate();
 
     const userIsAdmin = (curUser !== undefined && curUser.admin_privileges === true);
@@ -16,11 +16,31 @@ const MenuAdminPage = ({ curUser, allVinyls, setSelectedVinyls, addItemToShoppin
 
     if (!userIsAdmin) return null;
 
+    function showVinylInfo(vinylObject) {
+        return (
+            <li key={vinylObject.id}>
+                <h2>{vinylObject.title}</h2>
+                <p>Valor: R${vinylObject.price}.00</p>
+                <p>Disponível em estoque: {vinylObject.available_qty}</p>
+                <button>Editar</button>
+                <button onClick={() => removeVinyl(vinylObject.id)}>Remover</button>
+            </li>
+        );
+    };
+
+    function removeVinyl(vinylId) {
+        if (window.confirm("Confirmar exclusão do item?")) {
+            setAllVinyls(allVinyls.filter(vinyl => vinyl.id !== vinylId));
+        }
+    }
 
     return (
         <>
             <Header curUser={curUser}/>
             <h1 style={{margin: '20px 0px 0px 20px'}}>Menu administrador</h1>
+            <ul>
+                {[...allVinyls].map(vinylObject => showVinylInfo(vinylObject))}
+            </ul>
             <Footer />
         </>
     );
