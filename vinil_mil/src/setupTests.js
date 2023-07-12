66d5d71c41dafc33ad8cda2c167c9baf9e927c9b
thaislauriano31 <thaislauriano@usp.br>
2023-07-12 07:54:12 -0300

    comecando testes

--- vinil_mil/src/setupTests.js
+++ vinil_mil/src/setupTests.js
@@ -3,3 +3,26 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import App from './App'
+import AuthForm from '/src/components/AuthForm/index.jsx'
+
+
+import { render } from '@testing-library/react'
+
+describe('', () => {
+    it ('should be able to register as new user', () => {
+        const {} = render(<App />)
+    })
+})
+
+describe('', () => {
+    it ('should be able to add new product to the inventory', () => {
+        const {} = render(<App />)
+    })
+})
+
+describe('', () => {
+    it ('should be able to ', () => {
+        const {} = render(<App />)
+    })
+})
